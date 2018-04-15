94fd0b09eb0ddd7d2dc518467643117dfb022162
Amin <aminkh17@outlook.com>
2018-04-15 22:15:43 +0800

    finish all the requirements

--- jquery/fe_01.js
+++ jquery/fe_01.js
@@ -2,31 +2,53 @@
     var App = function(){
         var url = 'http://ui-grid.info/data/500_complex.json';
 
-        function view(e) {
+        const MSGSPAN = "#response-message";
+        const SUCCESS = "success";
+        const FAILED = "failed";
+        
+
+        function view(e, event) {
+            // OR > var id = $(event.parentElement.parentElement).find(">:first-child").text(); //please determine the id
+            var id = e;
             $.each(users, function(index, user){
-                if(e === user.id){
+                if(id === user.id){
                     $('.fullmodal').show();
                     for(var n in user){
                         $('#p'+n).text(user[n]);
                     }
                 }
             })
-            // var id = '??'; //please determine the id
+            // OR
             // $.ajax({
             //     url: url + '/view/' +id,
             //     data: data
             // }).done( function (data){
-            //});
+            // });
         }
 
-        function remove(event) {
-            var id = '??'; //please determine the id
-            $.ajax({
-                url: url + '/remove/' + id ,
-                data: data
-            }).done( function (data){
+        function remove(e) {
+            for(var i=0; i<users.length; i++)
+            {
+                var user = users[i];
+                if(e === user.id){
+                    var c = confirm("Do you want to delete this person: "+user.name);
+                    if(c){
+                        users.splice(i,1);
+                        createRows(users);
+                        showMessage("Person has been deleted.", SUCCESS);
+                        
+                    }
+                    return;
+                }
+            }
+            // OR
+            // var id = '??'; //please determine the id
+            // $.ajax({
+            //     url: url + '/remove/' + id ,
+            //     data: data
+            // }).done( function (data){
 
-            });
+            // });
         }
 
         function initialize() {
@@ -43,10 +65,27 @@
                     });
                 }
                 createRows(users);
+                showMessage("Request has been done", SUCCESS);
+            }).fail(function(err){
+                showMessage("Request is failed!", FAILED);
+                
             })
         }
 
+        function showMessage(msg, type){
+            $(MSGSPAN).addClass(type).text(msg).show();
+            setTimeout(() => {
+                hideMessage();
+            }, 5000);
+        }
+
+        function hideMessage(){
+            $(MSGSPAN).hide();
+        }
+
+        hideMessage();
         function createRows(users){
+            
             var table =  $('<tbody>');
             for(i=0; i<users.length; i++){
                 var row = $('<tr>');
@@ -55,12 +94,14 @@
                 }
                 row
                 .append($('<td>')
-                    .append($("<a>").attr("href","javascript:App().view("+users[i].id+")").text('View'))
-                    .append($("<a>").attr("href","javascript:remove("+users[i].id+")").text('Remove'))
+                    .append($("<a>").attr("href","#").attr("onclick","App().view("+users[i].id+", this)").text('View'))
+                    .append($("<a>").attr("href","#").attr("onclick","App().remove("+users[i].id+", this)").text('Remove'))
                 );
                 table.append(row);
             }
+            $('#gtable tbody').remove();
             $('#gtable').append(table);
+            
         }
 
         return {
--- jquery/readme.md
+++ jquery/readme.md
@@ -1,14 +1,17 @@
 # Front End Hand-ons Test
 
+This is a simple JQuery project. 
+
 ##Instruction
-You've provided with 2 source code:
+You will find three file as below:
 
 1. fe_01.html
 2. fe_01_style.css
+3. fe_01.js
 
-*fe_o1.html is not complete code where you'll complete it for this hands-on test*
-
-*all ajax url is not working, it just dummy url*
+*fe_01.html is not complete HTML code*
+*fe_01_style.css contains all CSS code*
+*fe_01.js contains all Javascript code*
 
 ##Introduction
 From the **Table** you can see a list with 5 column id, name, age, sex and action columns.
@@ -25,45 +28,4 @@ inside the **App** class some just declaration and some with a predefine logic.
 1. view method
 2. remove method
 3. initialize method
-
-
-###Questions
-1. Please make all action buttons (view and remove works) respectively
-    1. when view is click it will stub / call method **view** in **App** class
-    
-    2. when remove is click it will stub / call method **remove** in **App** class
-    
-    3. For each method (view, remove), for ajax url it need an id to identify which person is being used as 
-    parameters. you can check in the table in the html element what is the id for each person
-    which is display in column ID. Please identify the respectively *id* by replace the "??" [line 49 and 59] in each method with your single line code.
-    ```
-    var id = '??'; 
-    ```
-
-2. Make the UI flow works (for all #2 question you can add your own methods if you want)
-By default in the html there is element with id "response-message" which by default is not hidden.
-
-    1. Please make it hidden once the page is loaded, so by default user will not see this message when the page is
-    loaded. You can hide it with css or javascript.
-    
-    2. Lets assume the all the ajax request will return either *true* or *false* for success and 
-    failed respectively. foreach type of there is a predefined css style in **css** file which are
-    *.success* and *.failed* respectively.
-        1. Please complete **view** method on result *true* SHOW the *response-message* and the
-        elements should apply *.success* provided in css and show message "Request has been done" within the element
-        
-        2. Please complete **view** method on result *false* SHOW the *response-message* and the element should
-        apply *.failed* provided in the css and show message "Request is failed!" within the element.
-    
-        3. For each message display (in #response-message element), either *success* or *failed* it will just show to user in 5 seconds, then it
-        should hide the message automatically.
-        
-        4. Please complete **delete** method on result *success* it will remove which rows it was intended to remove.
-        for e.g if user click remove row of John Smith please remove the table row after *success*
-        
-3. Sass / Scss conversion
-From the given css file (fe_01_style.css), please add a file of sass / scss file which will having same result as the 
-css. 
-p/s Try utilize as many sass / scss feature when you converting the css.
-    
 
